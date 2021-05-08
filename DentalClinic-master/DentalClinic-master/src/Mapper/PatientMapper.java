f9e68b39667b4bbb1955083030c7f36c9e959943
Hien1207 <tranthihien12701@gmail.com>
2021-05-08 22:22:23 +0700

    pbl

---
+++ DentalClinic-master/DentalClinic-master/src/Mapper/PatientMapper.java
@@ -0,0 +1,33 @@
+package Mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import DTO.Patient;
+
+public class PatientMapper implements RowMapper<Patient>
+{
+
+	@Override
+	public Patient mapRow(ResultSet rs) {
+		// TODO Auto-generated method stub
+		Patient p = new Patient();
+		try {
+			p.setId(rs.getInt("id"));
+			p.setAddress(rs.getString("address"));
+			p.setBirthday(rs.getDate("birthday"));
+			p.setCreatedBy(rs.getString("created_by"));
+			p.setCreatedDate(rs.getTimestamp("created_date"));
+			p.setFullname(rs.getString("fullname"));
+			p.setGender(rs.getByte("gender"));
+			p.setiCard(rs.getString("icard"));
+			p.setModifiedBy(rs.getString("modified_by"));
+			p.setModifiedDate(rs.getTimestamp("modified_date"));
+			p.setPhone(rs.getString("phone"));
+		} catch (SQLException e) {
+			return null;
+		}
+		return p;
+	}
+
+}
