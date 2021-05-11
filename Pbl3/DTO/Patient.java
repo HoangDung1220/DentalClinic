88e17691e996f58c102fa28f5978ada5691ab9ef
Hien1207 <78089627+Hien1207@users.noreply.github.com>
2021-05-11 22:24:07 +0700

    Add files via upload

---
+++ Pbl3/DTO/Patient.java
@@ -0,0 +1,13 @@
+package DTO;
+
+public class Patient extends AbstractPerson<Patient>{
+	private int status;
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+}
