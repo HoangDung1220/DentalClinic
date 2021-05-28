b3e7e0b40f1c70bfbb14901f0e3e0e26d303ea2b
- <->
2021-05-28 23:25:19 +0700

    GUI MângerPatient

--- src/DAL/GenericDAL.java
+++ src/DAL/GenericDAL.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import Mapper.RowMapper;
 
-public interface GenericDAL<T> {
-	public  List<T> query(String st,RowMapper<T> rowMapper,Object... paramters);
-	public int insert(String st,Object... paramters);
-	public void update(String st,Object... paramters);
-	public void delete(String st,Object... paramters);
+public interface GenericDAL<T>
+{
+	public List<T> query(String st, RowMapper<T> rowMapper, Object...parameters);
+	public void insert(String st, Object...parameters);
+	public void update(String st, Object...parameters);
+	public void delete(String st, Object...parameters);
+}
 	
 
 }
